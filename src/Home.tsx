464af39f9b92b95dacc70bf22f6331f27a6b3aa1
Luca Casonato <hello@lcas.dev>
2021-06-08 22:50:30 +0200

    add release data

--- src/Home.tsx
+++ src/Home.tsx
@@ -4,14 +4,32 @@ import { Test } from "./data.ts";
 import { Footer } from "./Footer.tsx";
 import { Header } from "./Header.tsx";
 
-export function Home(props: { latest: string; latestTests: Test[] }) {
-  let passed = 0;
-  let total = 0;
+export function Home(
+  props: {
+    latest: string;
+    latestTests: Test[];
+    releaseName: string;
+    releaseCommit: string;
+    releaseTests: Test[];
+  },
+) {
+  let passedLatest = 0;
+  let totalLatest = 0;
 
   for (const test of props.latestTests) {
     for (const c of test.cases) {
-      passed += c.passed ? 1 : 0;
-      total++;
+      passedLatest += c.passed ? 1 : 0;
+      totalLatest++;
+    }
+  }
+
+  let passedRelease = 0;
+  let totalRelease = 0;
+
+  for (const test of props.releaseTests) {
+    for (const c of test.cases) {
+      passedRelease += c.passed ? 1 : 0;
+      totalRelease++;
     }
   }
 
@@ -31,10 +49,11 @@ export function Home(props: { latest: string; latestTests: Test[] }) {
         compatibility between engines. It ensures that web APIs behave the same
         across different browsers.
       </p>
-      <div class={tw`mt-4`}>
-        <p class={tw`mt-4`}>
+      <div class={tw`mt-6 border-t border-gray-200 pt-6`}>
+        <p>
           Ran <span class={tw`font-bold`}>{props.latestTests.length}</span>{" "}
-          WPT tests for latest `main` commit{" "}
+          WPT tests for latest <span class={tw`font-bold`}>main</span> commit
+          {" "}
           <a
             href={`https://github.com/denoland/deno/commits/${props.latest}`}
             class={tw`text-blue-500 hover:underline`}
@@ -43,7 +62,7 @@ export function Home(props: { latest: string; latestTests: Test[] }) {
           </a>.
         </p>
         <p class={tw`mt-4`}>
-          <Ratio passed={passed} total={total} />
+          <Ratio passed={passedLatest} total={totalLatest} />
         </p>
         <p class={tw`mt-4`}>
           <a
@@ -51,6 +70,32 @@ export function Home(props: { latest: string; latestTests: Test[] }) {
             class={tw`text-blue-500 hover:underline`}
           >
             View detailed data for latest commit ➔
+          </a>
+        </p>
+      </div>
+      <div class={tw`mt-6 border-t border-gray-200 pt-6`}>
+        <p>
+          Ran <span class={tw`font-bold`}>{props.releaseTests.length}</span>
+          {" "}
+          WPT tests for <span class={tw`font-bold`}>{props.releaseName}</span>
+          {" "}
+          release{" "}
+          <a
+            href={`https://github.com/denoland/deno/commits/${props.releaseCommit}`}
+            class={tw`text-blue-500 hover:underline`}
+          >
+            {props.releaseCommit.slice(0, 8)}
+          </a>.
+        </p>
+        <p class={tw`mt-4`}>
+          <Ratio passed={passedRelease} total={totalRelease} />
+        </p>
+        <p class={tw`mt-4`}>
+          <a
+            href={`/${props.releaseCommit}`}
+            class={tw`text-blue-500 hover:underline`}
+          >
+            View detailed data for {props.releaseName} release ➔
           </a>
         </p>
       </div>
--- src/handler.tsx
+++ src/handler.tsx
@@ -12,14 +12,29 @@ import { Data } from "./Data.tsx";
 import { wptDataForCommit } from "./data.ts";
 import { Home } from "./Home.tsx";
 
+const RELEASE_NAME = "v1.11.0";
+const RELEASE_COMMIT = "b98293e21cb7e55def8fea2ca015381150d4cebf";
+
 export const handler = router({
   "/": async () => {
+    const releaseTestsPromise = wptDataForCommit(RELEASE_COMMIT);
     const latest = await fetch("https://dl.deno.land/wpt-latest.txt")
       .then((r) => r.text()).then((t) => t.trim());
-    const latestTests = await wptDataForCommit(latest);
+    const [latestTests, releaseTests] = await Promise.all([
+      wptDataForCommit(latest),
+      releaseTestsPromise,
+    ]);
     const body = render(
       "Deno WPT Compat",
-      () => <Home latest={latest} latestTests={latestTests} />,
+      () => {
+        return <Home
+          latest={latest}
+          latestTests={latestTests}
+          releaseCommit={RELEASE_COMMIT}
+          releaseName={RELEASE_NAME}
+          releaseTests={releaseTests}
+        />;
+      },
     );
     return new Response(body, {
       headers: {
