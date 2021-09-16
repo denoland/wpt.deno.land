68a7899da604732b769b062fd2f239267db59c1a
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-16 23:41:32 +0800

    Delete deps.ts

--- deps.ts
+++
@@ -1,6 +0,0 @@
-export { Fragment, h } from "https://x.lcas.dev/preact@10.5.12/mod.js";
-export type { ComponentType } from "https://x.lcas.dev/preact@10.5.12/mod.js";
-export { renderToString } from "https://x.lcas.dev/preact@10.5.12/ssr.js";
-export { setup, tw } from "https://esm.sh/twind@0.16.16";
-export { getStyleTag, virtualSheet } from "https://esm.sh/twind@0.16.16/sheets";
-export { router } from "http://crux.land/router@0.0.2";
