57295167c5624242f4023d4bdc5be87fee63820d
Luca Casonato <hello@lcas.dev>
2022-01-20 17:12:41 +0100

    Just redirect to wpt.fyi

--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
-import { handler } from "./src/handler.tsx";
+#!/usr/bin/env -S deno run --allow-net --no-check
 
-addEventListener("fetch", (e) => {
-  e.respondWith(handler(e.request));
-});
+import { serve } from "https://deno.land/std@0.122.0/http/server.ts";
+
+const URL =
+  "https://wpt.fyi/results/?label=master&label=experimental&product=chrome&product=deno&product=firefox&product=safari&aligned";
+
+console.log("Listening at http://localhost:8000/");
+serve(() => Response.redirect(URL, 307));
